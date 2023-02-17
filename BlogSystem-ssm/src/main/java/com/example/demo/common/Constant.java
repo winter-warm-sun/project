cafa3fb9db526de8b0ebb42dbc67f6c10e5b8865
如风暖阳 <1491788197@qq.com>
2023-02-17 09:33:58 +0800

    searcher-index

--- BlogSystem-ssm/src/main/java/com/example/demo/common/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.common;
-
-/**
- * 当前项目中的所有常量
- */
-public class Constant {
-    // 登录信息存储到session中的key
-    public static final String SESSION_USERINFO_KEY="session_userinfo_key";
-}
