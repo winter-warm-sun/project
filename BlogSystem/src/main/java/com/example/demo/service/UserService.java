e8ae6aa69d2b4b07a8e90db5a1fa447d75747841
如风暖阳 <1491788197@qq.com>
2023-02-17 09:32:48 +0800

    博客系统

---
+++ BlogSystem/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.UserInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    public int add(String username, String password) {
+        return userMapper.add(username,password);
+    }
+
+    public UserInfo login(String username) {
+        return userMapper.login(username);
+    }
+
+    public UserInfo getAuthorInfo(Integer aid) {
+        return userMapper.getAuthorInfo(aid);
+    }
+}
---
+++ BlogSystem/src/main/resources/static/js/tools.js
@@ -0,0 +1,55 @@
+// 获取当前url中某个参数的方法
+function getURLParam(key) {
+    var params=location.search;
+    if(params.indexOf("?")>=0) {//如果存在查询字符“？”
+        params=params.substring(1);
+        let paramArr=params.split('&');
+        for(let i=0;i<paramArr.length;i++) {
+            let namevalues=paramArr[i].split("=");
+            if(namevalues[0]==key) {
+                return namevalues[1];
+            }
+        }
+    }else {
+        return "";
+    }
+}
+/*
+用于获取 URL 参数中指定 key 的值。
+首先，使用 location.search 获取 URL 中的参数部分。
+如果 URL 中存在参数（即 location.search 不为空，且包含 "?"），
+则使用 substring 和 split 方法将参数转换为一个数组。循环遍历数组，
+并使用 split 方法将每个参数拆分为 key 和 value，如果 key 与函数参数 key 相等，
+则返回该 value。如果没有找到匹配的 key，则返回空字符串。
+*/
+
+// 用于退出登录
+function onExit() {
+    if(confirm("确认退出？")) {
+        //ajax请求后端进行退出操作
+        jQuery.ajax({
+            url:"/user/logout",
+            type:"POST",
+            data:{},
+            success:function(result) {
+                location.href="/login.html";
+            },
+            error:function(err) {
+                if(err!=null&& err.status==401) {
+                    alert("用户未登录，即将跳转到登录页!");
+                    // 已经被拦截器拦截了，未登录
+                    location.href="/login.html"
+                }
+            }
+        });
+    }
+}
+
+var descLength=150; // 简介最大长度
+// 字符串截取,将文章正文截取成简介
+function mySubStr(content) {
+    if(content.length>descLength) {
+        return content.substr(0,descLength);
+    }
+    return content;
+}
