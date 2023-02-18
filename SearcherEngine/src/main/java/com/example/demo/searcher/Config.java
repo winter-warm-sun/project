09340c8e32e8e319e66d6868016a8b32f3a72f3a
如风暖阳 <1491788197@qq.com>
2023-02-18 12:05:08 +0800

    。

--- SearcherEngine/src/main/java/com/example/demo/searcher/Config.java
+++ SearcherEngine/src/main/java/com/example/demo/searcher/Config.java
@@ -2,5 +2,5 @@ package com.example.demo.searcher;
 
 public class Config {
     // 这个变量为true，表示是在云服务器上运行的；为false表示在本地运行
-    public static boolean isOnline=false;
+    public static boolean isOnline=true;
 }
